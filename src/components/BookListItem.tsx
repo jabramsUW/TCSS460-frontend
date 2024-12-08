f364d418b2eace6d2bad9adfb1b689d1300ba8b0
Dustin <103460016+DFeldt5@users.noreply.github.com>
2024-12-08 13:11:21 -0800

    Set bookListItem as buttons

--- src/components/BookListItem.tsx
+++ src/components/BookListItem.tsx
@@ -1,7 +1,7 @@
 import DeleteIcon from '@mui/icons-material/Delete';
 import CommentsDisabledIcon from '@mui/icons-material/CommentsDisabled';
-import { IconButton, ListItem, ListItemAvatar, ListItemText, Rating } from '@mui/material';
-
+import { IconButton, ListItem, ListItemAvatar, ListItemButton, ListItemText, Rating } from '@mui/material';
+import NextLink from 'next/link';
 import { IBook } from 'types/book';
 import Avatar from './@extended/Avatar';
 
@@ -14,22 +14,25 @@ export function BookListItem({ book, onDelete }: { book: IBook; onDelete: (isbn:
         </IconButton>
       }
     >
-      <ListItemAvatar>
-        <Avatar alt={book.title} src={book.icons.small} variant="square" />
-      </ListItemAvatar>
-      <ListItemText
-        primary={book.title}
-        secondary={
-          <div>
-            <div>{book.authors}</div>
+      <ListItemButton>
+        <NextLink href={`/books/view?${book.isbn13}`} passHref legacyBehavior />
+        <ListItemAvatar>
+          <Avatar alt={book.title} src={book.icons.small} variant="square" />
+        </ListItemAvatar>
+        <ListItemText
+          primary={book.title}
+          secondary={
             <div>
-              <Rating name="bookRating" value={book.ratings.average} precision={0.2} size="small" readOnly />
+              <div>{book.authors}</div>
+              <div>
+                <Rating name="bookRating" value={book.ratings.average} precision={0.2} size="small" readOnly />
+              </div>
+              <div>{book.publication}</div>
             </div>
-            <div>{book.publication}</div>
-          </div>
-        }
-        secondaryTypographyProps={{ color: 'gray' }}
-      />
+          }
+          secondaryTypographyProps={{ color: 'gray' }}
+        />
+      </ListItemButton>
     </ListItem>
   );
 }
